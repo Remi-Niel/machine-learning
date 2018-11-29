6bf5e2a85e52859863e8fb0c006bc1a2c464eeb3
Remi <r.f.niel@student.rug.nl>
2018-11-29 13:54:40 +0100

    test

--- wave-net/getbatch.py
+++ wave-net/getbatch.py
@@ -38,7 +38,7 @@ def getBatch(size = 100):
 
 		labels.append(label_indexes[file_name.split('/')[1]]);
 	
-	data_labels = numpy.array(data_labels);
+	data_labels = np.array(data_labels);
 
 	return (data,one_hot(labels,NUM_LABELS))
 
