bb31b4e1d776b8ab21c49c4b2bb65253719f1c40
Remi <r.f.niel@student.rug.nl>
2018-11-29 15:23:20 +0100

    test

--- wave-net/network.py
+++ wave-net/network.py
@@ -55,9 +55,10 @@ EPOCHS = 50
 for i in range(1):
     (x_train, y_train) = getbatch.getBatch(1,True)
     x_train = x_train.astype("float32") / 32768
-    print(x_train);
     y_train = y_train.astype("float32")
     y_train = y_train[:,2]
+    print(x_train);
+    print(y_train);
     history = model_m.train_on_batch(x_train,y_train)
 
 print("\n--- Check against test data ---\n")
