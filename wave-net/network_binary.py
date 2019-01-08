c067ffa77b7148b6e7f7f825a52219ac5407ee4a
Remi <r.f.niel@student.rug.nl>
2019-01-08 11:36:47 +0100

    training models

--- wave-net/network_binary.py
+++ wave-net/network_binary.py
@@ -27,22 +27,31 @@ model_m.add(MaxPooling1D(2))
 model_m.add(Conv1D(64, 2, strides = 1, activation='relu'))
 model_m.add(MaxPooling1D(2))
 
-model_m.add(Conv1D(64, 2, strides = 2, activation='relu'))
+model_m.add(Conv1D(64, 2, strides = 1, activation='relu'))
 model_m.add(MaxPooling1D(2))
 
-model_m.add(Conv1D(64, 2, strides = 2, activation='relu'))
+model_m.add(Conv1D(64, 2, strides = 1, activation='relu'))
 model_m.add(MaxPooling1D(2))
 
-model_m.add(Conv1D(64, 2, strides = 2, activation='relu'))
+model_m.add(Conv1D(64, 2, strides = 1, activation='relu'))
 model_m.add(MaxPooling1D(2))
 
-model_m.add(Conv1D(64, 2, strides = 2, activation='relu'))
+model_m.add(Conv1D(64, 2, strides = 1, activation='relu'))
 model_m.add(MaxPooling1D(2))
 
-model_m.add(Conv1D(64, 2, strides = 2, activation='relu'))
+model_m.add(Conv1D(64, 2, strides = 1, activation='relu'))
 model_m.add(MaxPooling1D(2))
 
-model_m.add(Conv1D(256, 2, strides = 2, activation='relu'))
+model_m.add(Conv1D(64, 2, strides = 1, activation='relu'))
+model_m.add(MaxPooling1D(2))
+
+model_m.add(Conv1D(64, 2, strides = 1, activation='relu'))
+model_m.add(MaxPooling1D(2))
+
+model_m.add(Conv1D(64, 2, strides = 1, activation='relu'))
+model_m.add(MaxPooling1D(2))
+
+model_m.add(Conv1D(256, 2, strides = 1, activation='relu'))
 model_m.add(Flatten())
 
 #model_m.add(Dense(512))
