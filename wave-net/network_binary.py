a448680774ae316b3bdc6b31207d41831e7f38eb
Remi <r.f.niel@student.rug.nl>
2019-01-08 13:58:34 +0100

    home

--- wave-net/network_binary.py
+++ wave-net/network_binary.py
@@ -52,7 +52,7 @@ for CLASS in range(11):
     # if it fails to improve for ten consecutive epochs,
     # training stops early
     callbacks_list = [
-        keras.callbacks.EarlyStopping(monitor='val_acc', patience=20, restore_best_weights = True, verbose = 1),
+        keras.callbacks.EarlyStopping(monitor='val_acc', patience=10, restore_best_weights = True, verbose = 1),
 	keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=2, min_lr=0.000001)
     ]
 
