d9f3b0ebb07b7bc996fe25aa6719915e9b95195a
Remi <r.f.niel@student.rug.nl>
2019-01-08 11:51:07 +0100

    training models

--- wave-net/network_binary.py
+++ wave-net/network_binary.py
@@ -21,37 +21,28 @@ input_shape = (TIME_PERIODS*num_sensors)
 # 1D CNN neural network
 model_m = Sequential()
 model_m.add(Reshape((TIME_PERIODS, num_sensors),  input_shape=(input_shape,)))
-model_m.add(Conv1D(128, 3, strides = 1, activation='relu', input_shape=(TIME_PERIODS, num_sensors)))
+model_m.add(Conv1D(64, 2, strides = 2, activation='relu', input_shape=(TIME_PERIODS, num_sensors)))
 model_m.add(MaxPooling1D(2))
 
-model_m.add(Conv1D(128, 3, strides = 1, activation='relu'))
+model_m.add(Conv1D(64, 2, strides = 2, activation='relu'))
 model_m.add(MaxPooling1D(2))
 
-model_m.add(Conv1D(128, 3, strides = 1, activation='relu'))
+model_m.add(Conv1D(32, 2, strides = 2, activation='relu'))
 model_m.add(MaxPooling1D(2))
 
-model_m.add(Conv1D(128, 3, strides = 1, activation='relu'))
+model_m.add(Conv1D(32, 2, strides = 2, activation='relu'))
 model_m.add(MaxPooling1D(2))
 
-model_m.add(Conv1D(128, 3, strides = 1, activation='relu'))
+model_m.add(Conv1D(64, 2, strides = 2, activation='relu'))
 model_m.add(MaxPooling1D(2))
 
-model_m.add(Conv1D(128, 3, strides = 1, activation='relu'))
+model_m.add(Conv1D(32, 2, strides = 2, activation='relu'))
 model_m.add(MaxPooling1D(2))
 
-model_m.add(Conv1D(128, 3, strides = 1, activation='relu'))
+model_m.add(Conv1D(32, 2, strides = 2, activation='relu'))
 model_m.add(MaxPooling1D(2))
 
-model_m.add(Conv1D(128, 3, strides = 1, activation='relu'))
-model_m.add(MaxPooling1D(2))
-
-model_m.add(Conv1D(128, 3, strides = 1, activation='relu'))
-model_m.add(MaxPooling1D(2))
-
-model_m.add(Conv1D(128, 3, strides = 1, activation='relu'))
-model_m.add(MaxPooling1D(2))
-
-model_m.add(Conv1D(256, 3, strides = 1, activation='relu'))
+model_m.add(Conv1D(256, 2, strides = 2, activation='relu'))
 model_m.add(Flatten())
 
 #model_m.add(Dense(512))
