2b1ecae692a9540c094ced55b1c5e6aa0369e446
Remi <r.f.niel@student.rug.nl>
2019-01-11 13:03:13 +0100

    loading test files and models

--- wave-net/test.py
+++ wave-net/test.py
@@ -13,7 +13,7 @@ from keras import backend as K
 def getinput(file_name):
 	(sample_rate, signal) = wavfile.read(file_name)
 
-	print(signal[0])
+	print(signal.shape)
 	Nsamp = math.floor(signal.shape().first/44100)
 
 	inputs = np.zeros((int(Nsamp),44100))
