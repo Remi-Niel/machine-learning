94b3a977e72ae64f48810d7958617e2eed329af8
Remi <r.f.niel@student.rug.nl>
2019-01-11 16:21:31 +0100

    loading test files and models

--- wave-net/test.py
+++ wave-net/test.py
@@ -27,7 +27,7 @@ def determineOptimalThreshold(groundTmean, groundFmean):
 		FP = max(1,sum(1 for x in groundFmean if x >= t))
 		TN = max(1,len(groundFmean) - FP)
 
-		tmp = (TP / (TP + FP)) * (TP / (TP + FN));
+		tmp = (TP / (TP + FP)) + (TP / (TP + FN));
 		if (tmp > f):
 			best_TP = TP;
 			best_FP = FP;
