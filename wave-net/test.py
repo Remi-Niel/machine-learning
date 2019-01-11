838e84ce0bf4832cdaebbf49110680b2d13636e3
Remi <r.f.niel@student.rug.nl>
2019-01-11 13:04:41 +0100

    loading test files and models

--- wave-net/test.py
+++ wave-net/test.py
@@ -29,7 +29,7 @@ def getinput(file_name):
 
 	print(len(mono))
 
-	for i in range(Nsamp):
+	for i in range(Nsamp-1):
 		sample = signal[Nsamp*44100:(Nsamp + 1)*44100 - 1]
 		print(len(signal))
 		inputs[i,:] = sample
