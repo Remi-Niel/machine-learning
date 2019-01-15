c8f969c7fbc65af74813f58659d68d11a5c85612
Remi <r.f.niel@student.rug.nl>
2019-01-15 09:33:52 +0100

    home

--- spectogram-CNN/network_binary.py
+++ spectogram-CNN/network_binary.py
@@ -21,10 +21,10 @@ IMSIZE = 128
 model_m = Sequential()
 model_m.add(Reshape((IMSIZE, IMSIZE,1),  input_shape=(IMSIZE,IMSIZE)))
 
-model_m.add(Conv2D(32, kernel_size = 3, activation='relu', padding = 'valid'))
+model_m.add(Conv2D(32, kernel_size = 7, activation='relu', padding = 'valid'))
 model_m.add(MaxPooling2D(2))    
 
-model_m.add(Conv2D(32, kernel_size = 3, activation='relu', padding = 'valid'))
+model_m.add(Conv2D(32, kernel_size = 7, activation='relu', padding = 'valid'))
 model_m.add(MaxPooling2D(2))
 
 model_m.add(Conv2D(64, kernel_size = 3, activation='relu', padding = 'valid'))
