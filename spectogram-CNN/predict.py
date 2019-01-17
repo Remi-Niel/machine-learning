d08cc950dfbd5d16df9ca2e71063f5ee77a22fdc
Remi <r.f.niel@student.rug.nl>
2019-01-17 13:41:53 +0100

    prep demo

---
+++ spectogram-CNN/predict.py
@@ -0,0 +1,73 @@
+import os
+import glob
+import sys
+import progressbar
+import numpy as np
+from scipy.io import wavfile
+import scipy
+from matplotlib import mlab
+import math
+import keras
+import random
+from keras.models import load_model
+from keras import backend as K 
+from tensorflow import Session
+import getbatch_binary as getbatch
+
+
+def getinput(file_name, Nsamp = 100):
+	(sample_rate, signal) = wavfile.read(file_name)
+
+	Nsamp = min(math.floor(signal.shape[0]/44100), Nsamp) 
+
+	mono = signal.sum(axis = 1) / 2
+
+	mean = np.mean(mono) # is about zero
+	stddev = np.std(mono)
+	if stddev == 0:
+		stddev = 1
+
+	mono = (mono - mean) / stddev
+
+	
+
+	IMSIZE = 128
+
+	inputs = np.zeros((int(Nsamp), IMSIZE, IMSIZE))
+
+
+	for i in range(Nsamp) :
+		subsample = mono[(i*44100):(i+1)*44100]
+
+		spec = mlab.specgram(subsample, Fs = 44100)[0]
+
+		spec = scipy.misc.imresize(spec, [IMSIZE,IMSIZE])
+
+		inputs[i,:,:] = spec
+	
+	return inputs
+
+
+model = load_model("multi_model/model.model")
+
+label_set = getbatch.labels;
+
+wav_file = sys.argv[1];
+
+input = getinput(wav_file)
+
+highestActivation = -1;
+bestGuess = -1;
+
+prediction = model.predict(input)
+
+summed = np.sum(prediction, axis = 0)
+
+
+best = np.argmax(summed)
+
+label = label_set[best]
+
+print("Recognised instrument: " + label)
+print("Certainty: " + str(best/np.sum(summed)))
+
